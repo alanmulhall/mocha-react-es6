bab63b5babbe6131e6141d04ae450cdcf9ea5636
Alan Mulhall <alan.mulhall@nfl.com>
2015-10-13 20:31:22 -0700

    Fix depreciation notices

--- app/HelloWorld.spec.js
+++ app/HelloWorld.spec.js
@@ -1,7 +1,8 @@
 // http://willcodefor.beer/react-testing-with-mocha-chai-sinon-and-gulp/
 // https://github.com/robertknight/react-testing
 
-import React from "react/addons";
+import ReactTestUtils from "react-addons-test-utils";
+import ReactDOM from "react-dom";
 import chai from "chai";
 import HelloWorld from "./HelloWorld";
 import setup from "./setup";
@@ -9,13 +10,11 @@ import setup from "./setup";
 const expect = chai.expect;
 
 describe("HelloWorld", () => {
-
     it("renders 'Hello World!'", () => {
         const component = React.addons.TestUtils.renderIntoDocument(
             <HelloWorld />
         );
-        const greeting = React.findDOMNode(component.refs.greeting);
-
+        const greeting = ReactDOM.findDOMNode(component.refs.greeting);
         expect(greeting.textContent).to.equal("Hello World");
     });
 });
